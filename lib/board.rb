94e158c7ce109564f0745b8d47485daae4f9c0d2
JFAldridge <jfaldridge1985@gmail.com>
2019-08-17 21:33:43 -0400

    Complete set_board

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,16 @@
 class Board
   def initialize
-    @current_state = Array.new(8) { Array.new(8, 0)}
+    @current_state = []
+    set_board
+  end
+
+  attr_accessor :current_state
+  
+  def set_board
+    @current_state.push([Rook.new('black'), Knight.new('black'), Bishop.new('black'), Queen.new('black'), King.new('black'), Bishop.new('black'), Knight.new('black'), Rook.new('black')])
+    @current_state.push([Pawn.new('black'), Pawn.new('black'), Pawn.new('black'), Pawn.new('black'), Pawn.new('black'), Pawn.new('black'), Pawn.new('black'), Pawn.new('black')])
+    4.times {@current_state.push(Array.new(8, 0))}
+    @current_state.push([Pawn.new('white'), Pawn.new('white'), Pawn.new('white'), Pawn.new('white'), Pawn.new('white'), Pawn.new('white'), Pawn.new('white'), Pawn.new('white')])
+    @current_state.push([Rook.new('white'), Knight.new('white'), Bishop.new('white'), Queen.new('white'), King.new('white'), Bishop.new('white'), Knight.new('white'), Rook.new('white')])
   end
 end
