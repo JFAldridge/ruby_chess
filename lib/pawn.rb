bf09b3d7b9307e8c40a0034dfa761bbb2399c40b
JFAldridge <jfaldridge1985@gmail.com>
2019-08-22 09:54:40 -0400

    Add is_move_allowed method to Pawn class

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -3,25 +3,42 @@ require_relative 'pieces.rb'
 class Pawn < Pieces
   def initialize(b_or_w, board)
     super(b_or_w, board)
+    @y_move = @b_or_w == 'white' ? -1 : 1
   end
 
-  attr_accessor :b_or_w
-
-  def move_allowed(loc, dest)
-    diagonal = [-1, 1]
-    y_move = b_or_w == 'white' ? -1 : 1
-
-    possible_x_paths = []
-
-    possible_x_paths.push(0) if @board[loc[0] + y_move][loc[1]] == 0
-
-    diagonal.each do |d| 
-      next if @board[loc[0] + y_move][loc[1] + d] == 0
-      possible_x_paths.push(d) if opponent_piece((loc[0] + y_move), (loc[1] + d)) 
-    end
+  def is_move_allowed?(loc, dest)
+    allowed_moves = find_allowed_moves(loc[0], loc[1])
+    allowed_moves.include?(dest)
   end
 
-  def opponent_piece(y, x)
-    @board[y][x].b_or_w == @b_or_w ? true : false
+  def find_allowed_moves(y, x)
+    move_list = []
+    move_list.push([y + @y_move, x]) if open_space?(y + @y_move, x)
+    move_list.push([y + @y_move, x - 1]) if opponent_piece(y + @y_move, x - 1)
+    move_list.push([y + @y_move, x + 1]) if opponent_piece(y + @y_move, x + 1)
+    move_list.push([y + @y_move * 2, x]) if two_forward_allowed?(y, x)
+  end
+
+  def two_forward_allowed?(y, x)
+    return false unless is_first_move?(y)
+    return false unless open_space?(y + @y_move, x) && open_space?(y + @y_move * 2, x)
+    true
+  end
+
+  def is_first_move?(y)
+    return true if @b_or_w == 'black' && y == 1 || @b_or_w == 'white' && y == 6
+    false
+  end
+
+  def open_space?(y,x)
+    @board.current_state[y][x] == 0
+  end
+
+  def opponent_piece?(y, x)
+    @board.current_state[y][x].b_or_w == @b_or_w 
+  end
+
+  def ally_piece?(y, x)
+    @board.current_state[y][x].b_or_w != @b_or_w 
   end
 end
