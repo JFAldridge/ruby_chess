71807b907e350a0cc7b422716afe6db22c9d87c0
JFAldridge <jfaldridge1985@gmail.com>
2019-08-23 12:01:34 -0400

    Add #is_move_allowed? to Queen/tests created

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,7 +1,16 @@
 require_relative 'pieces.rb'
 
 class Queen < Pieces
+  DIRECTIONS = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [1, -1], [-1, -1]]
+
   def initialize(b_or_w, board)
     super(b_or_w, board)
   end
+
+  def is_move_allowed?(loc, dest)
+    allowed_moves = find_allowed_moves_crossboard(loc[0], loc[1], DIRECTIONS)
+    
+    allowed_moves.include?(dest)
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -128,3 +128,35 @@ describe Rook do
     end
   end
 end
+
+describe Queen do
+  before { @board = Board.new }
+
+  describe '#is_move_allowed?' do
+  
+    describe 'rejects invalid move' do
+      it 'four spaces forward when blocked by pawn' do
+        expect(@board.current_state[0][3].is_move_allowed?([0, 3], [4, 3])).to eq(false)
+      end
+      it 'four spaces diagonally when blocked by pawn' do
+        expect(@board.current_state[0][3].is_move_allowed?([0, 3], [4, 7])).to eq(false)
+      end
+    end
+
+    describe 'accepts valid move' do
+      it 'four spaces forward when spaces are open' do
+        @board.current_state[1][3] = 0
+        expect(@board.current_state[0][3].is_move_allowed?([0, 3], [4, 3])).to eq(true)
+      end
+      it 'four spaces diagonally when spaces are open' do
+        @board.current_state[1][4] = 0
+        expect(@board.current_state[0][3].is_move_allowed?([0, 3], [4, 7])).to eq(true)
+      end
+      it 'four spaces diagonally to take pawn' do
+        @board.current_state[1][4] = 0
+        @board.current_state[4][7] = @board.current_state[6][7]
+        expect(@board.current_state[0][3].is_move_allowed?([0, 3], [4, 7])).to eq(true)
+      end
+    end
+  end
+end
