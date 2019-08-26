a7ad48d57b761a5e63b407664660ef3608bd1f65
JFAldridge <jfaldridge1985@gmail.com>
2019-08-26 14:46:10 -0400

    Create Interface module and it's #get_player_comp

---
+++ lib/interface.rb
@@ -0,0 +1,48 @@
+module Interface
+
+  def get_player_comp_and_names
+    players = []
+    player_comp = nil
+    player_one_name = nil
+    player_two_name = nil
+    
+    puts "Would you like to play against the computer or another human?"
+    
+    until player_comp 
+      puts "Enter 1 for computer or 2 for human"
+      input = gets.chomp.to_i
+      player_comp = input if input == 1 || input == 2
+    end
+    
+    if player_comp == 1
+      puts "What's your name?"
+    else
+      puts "What is player one's name?"
+    end
+  
+    name_1 = gets.chomp
+    if name_1 == ''
+      puts "We'll call you \"Babababababababa\" for simplicities sake."
+      name_1 = "Babababababababa"
+    end
+    players.push([name_1, 'human'])
+  
+    if player_comp == 1
+      puts
+      puts "Your opponent will be \"Blip,\" a toaster with aspirations."
+      players.push(["Blip", 'computer'])
+    else
+      puts
+      puts "What is play two's name?"
+      name_2 = gets.chomp
+      if name_2 == '' || name_2 == name_1
+      puts "Let's call you \"Pie.\""
+      name_2 = "Pie"
+    end
+      players.push([name_2, 'human'])
+    end
+  
+    players
+  end
+
+end
